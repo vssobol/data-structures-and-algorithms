c2c43b82affed6c0bab13f4d09ea22a9578405aa
vssobol <vssobol@gmail.com>
2019-07-24 20:38:05 -0700

    test 3 passes

--- code-challenges/challenges-04.test.js
+++ code-challenges/challenges-04.test.js
@@ -25,9 +25,7 @@ CHALLENGE 3
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
-const citiesAtoJ = (arr) => {
-    //code
-};
+const citiesAtoJ = (arr) => arr.filter(element => element.match(/^[A-J]\w+/));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
